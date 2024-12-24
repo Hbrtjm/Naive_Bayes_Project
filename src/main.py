1baa73a89e61ada24a252bb854c8b302a4237c07
Hbrtjm <hubi.hey@vp.pl>
2024-12-24 01:47:54 +0100

    I have 200 lines divine intellect naive Bayes classifier, you are a jolly child, have merry Christmas

--- src/main.py
+++ src/main.py
@@ -5,3 +5,6 @@ if __name__ == "__main__":
     nb = NaiveBayes()
     nb.set_data_source('mushrooms.csv')
     nb.fit()
+    data = ('b','y','n','f','p','f','w','b','n','t','c','s','s','w','w','p','w','o','e','k','s','m')
+    className, probability = nb.predict(data)
+    print(f"Object classified as \"{className}\" with probability {probability}")
--- src/naiveBayes.py
+++ src/naiveBayes.py
@@ -1,22 +1,31 @@
 import pandas as pd
-import numpy as np
+# import numpy as np
 from typing import Self, List
 from os import path
 from sys import stderr
+from sklearn.model_selection import train_test_split
+
 class NaiveBayes:
+
     def __init__(self: Self) -> None:
         """
         Constructor of the Naive Bayes class
         """
+        self.testTrainSplit = 0.2
         self.dataSize = None
+        self.classes = set()
+        self.traits = {}
+        self.traitNames = []
+        self.traitsConditionalLikelyhood = {}
         self.traitsCount = {}
         self.traitsLikelyhood = {}
+        self.traitsConditionalCount = {}
         self.classesCount = {}
         self.classesLikelyhood = {}
-        self.alfa = 1
+        self.alpha = 1
         self.current_dir = path.dirname(__file__)
         self.dataPath = self.current_dir
-        
+    
     def set_data_source(self: Self,filename: str,separator: str = ',') -> None:
         """
         Sets the data source for the model to train on
@@ -32,16 +41,32 @@ class NaiveBayes:
         self.dataPath = path.abspath(self.relativePath)
         self.separator = separator
     
-    def addClassValue(self: Self, value: str) -> None:
+    def add_trait_names(self: Self, df: pd.DataFrame):
+        for column in df.columns[1:]:
+            self.traits[column] = set()
+
+    def add_traits(self: Self, df: pd.DataFrame):
+        """
+        Updates the traits dictionary with all of the available traits, especially for the missing traits
+        """
+        for traitName in df.columns[1:]:
+            self.traitNames.append(traitName)
+        for index, row in df.iterrows():
+            for traitName in df.columns[1:]:
+                self.traits[traitName].add(row[traitName])
+
+    def add_class_value(self: Self, value: str) -> None:
         """
         Adds a new class to classesCount or incements the amount of the existing class by 1
         """
+        if value not in self.classes:
+            self.classes.add(value)
         if value not in self.classesCount:
             self.classesCount[value] = 1
         else:
             self.classesCount[value] += 1
 
-    def addTraitValues(self: Self, df: pd.DataFrame, row) -> None:
+    def add_trait_values(self: Self, df: pd.DataFrame, row) -> None:
         """
         For each trait function counts the occurence of its values in the dataset
         """
@@ -54,8 +79,38 @@ class NaiveBayes:
                 self.traitsCount[column][row[column]] = 1
                 continue
             self.traitsCount[column][row[column]] += 1
+
+    def add_conditional_likelyhood(self: Self, df: pd.DataFrame, row) -> None:
+        objectClass = row[df.columns[0]] 
+        if objectClass not in self.traitsConditionalCount:
+            self.traitsConditionalCount[objectClass] = {}
+        for column in df.columns[1:]:
+            if column not in self.traitsConditionalCount[objectClass]:
+                self.traitsConditionalCount[objectClass][column] = { row[column]:1 }
+                continue
+            if row[column] not in self.traitsConditionalCount[objectClass][column]:
+                self.traitsConditionalCount[objectClass][column][row[column]] = 1
+                continue
+            self.traitsConditionalCount[objectClass][column][row[column]] += 1
     
-    def calculateTraitLikelyhoods(self: Self, df: pd.DataFrame) -> None:
+    def add_alpha_to_traits(self: Self) -> None:
+        for traitName in self.traitNames: # Can be done using keys, but it's for clarity
+            for trait in self.traits[traitName]:
+                if trait not in self.traitsCount[traitName]:
+                    self.traitsCount[traitName][trait] = self.alpha
+                    continue
+                self.traitsCount[traitName][trait] += self.alpha
+
+    def add_alpha_to_conditional_traits(self: Self) -> None:
+        for className in self.classes:
+            for traitName in self.traitNames: # Can be done using keys, but it's for clarity
+                for trait in self.traits[traitName]:
+                    if trait not in self.traitsConditionalCount[className][traitName]:
+                        self.traitsConditionalCount[className][traitName][trait] = self.alpha
+                        continue
+                    self.traitsConditionalCount[className][traitName][trait] += self.alpha
+    
+    def calculate_trait_likelyhoods(self: Self, df: pd.DataFrame) -> None:
         """
         Calculates likelyhoods of all values of all traits
         """
@@ -64,15 +119,26 @@ class NaiveBayes:
             for key,value in self.traitsCount[column].items():
                 if column not in self.traitsLikelyhood:
                     self.traitsLikelyhood[column] = {}
-                self.traitsLikelyhood[column][key] = value/self.dataSize 
-            
-    def calculateClassesLikelyhoods(self: Self) -> None:
+                self.traitsLikelyhood[column][key] = value/(self.dataSize+self.alpha*len(self.traitsLikelyhood[column])) 
+
+    def calculate_classes_likelyhoods(self: Self) -> None:
         """
         Calculates likelyhoods of each class of all classes
         """
         for key,value in self.classesCount.items():
             self.classesLikelyhood[key] = value/self.dataSize
             print(f"{value} / {self.dataSize}")
+
+    def calculate_conditional_likelyhoods(self: Self, df: pd.DataFrame) -> None:
+        for className in self.classes:
+            if className not in self.traitsConditionalLikelyhood:
+                self.traitsConditionalLikelyhood[className] = {}
+            for column in df.columns[1:]:
+                # Each column has to have a value in this case, it will throw an error
+                for key,value in self.traitsConditionalCount[className][column].items():
+                    if column not in self.traitsConditionalLikelyhood[className]:
+                        self.traitsConditionalLikelyhood[className][column] = {}
+                    self.traitsConditionalLikelyhood[className][column][key] = value/(self.classesCount[className]+self.alpha*len(self.traitsConditionalLikelyhood[className][column]))
 
     def fit(self: Self) -> bool:
         """
@@ -95,28 +161,68 @@ class NaiveBayes:
         except Exception as error:
             print("An error has occured while reading the file (check if the file exists)", file=stderr)
             raise error
-        
-        self.dataSize = df.size//len(df.columns)
+        trainSet, testSet = train_test_split(df, test_size=self.testTrainSplit)
+        self.add_trait_names(trainSet)
+        self.add_traits(df)
+        self.dataSize = trainSet.size//len(trainSet.columns)
         # Set the class column name (usually 'class')
-        classColumnName = df.columns[0]
+        classColumnName = trainSet.columns[0]
         # Count classes and traits
-        for index, row in df.iterrows():
-            self.addClassValue(row[classColumnName])
-            self.addTraitValues(df,row)
+        for index, row in trainSet.iterrows():
+            self.add_class_value(row[classColumnName])
+            self.add_trait_values(trainSet,row)
+            self.add_conditional_likelyhood(trainSet,row)
+        
+        self.add_alpha_to_traits()
+        self.add_alpha_to_conditional_traits()
 
         # =============== Calculate likelyhoods ===============
-        self.calculateTraitLikelyhoods(df)
-        self.calculateClassesLikelyhoods()
-        print(self.traitsLikelyhood)
-        print(self.classesLikelyhood)
+        self.calculate_trait_likelyhoods(trainSet)
+        self.calculate_classes_likelyhoods()
+        self.calculate_conditional_likelyhoods(trainSet)
 
-        # =============== Train model ===============
-        # TODO
+        # =============== Test after training ===============
+        print("Testing after training")
+        correctGuesses = 0
+        adjustmentGuesses = 0
+        for rowIndex, test in testSet.iterrows():
+            # print(test)
+            className, likelyhoodValue = self.predict(test[1:])
+            if className == test[0]:
+                correctGuesses += 1
+                adjustmentGuesses += 1 # Didn't need adjustment
+            else:
+                predictions = self.predict_proba(test[1:])
+                # print(f"Wrong predictions {predictions} expected {test[0]} but gotten {className}")
+                newPredictions = self.make_mushroom_safe(predictions)
+                newClassName, _ = self.select_most_likely(newPredictions)
+                if newClassName == test[0]:
+                    adjustmentGuesses += 1
+        accuracy = correctGuesses/len(testSet)
+        adjustmentAccuracy = adjustmentGuesses/len(testSet)
+        print(f"Accuracy of {accuracy} accuracy after safety adjsutment {adjustmentAccuracy}") 
 
         # The model has been fit successfully
         return True
     
-    def predict(self: Self) -> tuple[str,float]:
+    def make_mushroom_safe(self: Self, predictions: dict[str,float]):
+        # ONLY FOR MUSHROOM PREDICTOR
+        if 'e' in predictions:
+            if predictions['e'] < 0.2: # Should not be considered safe to eat
+                predictions['e'] = 0
+        return predictions
+
+    def select_most_likely(self: Self, predictedProbabilities: dict[str,float]) -> tuple[str,float]:
+        maxProb = 0
+        maxClass = None
+        for key in predictedProbabilities.keys():
+            value =  predictedProbabilities[key] 
+            if value > maxProb:
+                maxProb = value
+                maxClass = key
+        return maxClass, maxProb
+
+    def predict(self: Self, dataRow: tuple[str]) -> tuple[str,float]:
         """
         Predicts which value is most likely to appear
         Arguments:
@@ -126,10 +232,10 @@ class NaiveBayes:
            > A string with class that has been chosen as the most likely
            > Probability of that class in the inverval [0,1]
         """
-        # TODO
-        pass
-
-    def predict_proba(self: Self) -> List[float]:
+        predictedProbabilities = self.predict_proba(dataRow)
+        return self.select_most_likely(predictedProbabilities)
+  
+    def predict_proba(self: Self,dataRow: tuple[str]) -> dict[str,float]:
         """
         Predicts which value is most likely to appear
         Arguments:
@@ -137,5 +243,13 @@ class NaiveBayes:
         Returns:
          - A list of probabilities of classification
         """
-        # TODO
-        pass
+        classProbabilities = {}
+        for className in self.classes:
+            resultProbability = 0
+            accumulatedTraits = 1
+            for i,trait in enumerate(dataRow):
+                # One-liner to be split up
+                accumulatedTraits *= self.traitsConditionalLikelyhood[className][self.traitNames[i]][trait]/self.traitsLikelyhood[self.traitNames[i]][trait]
+            resultProbability = accumulatedTraits*self.classesLikelyhood[className]
+            classProbabilities[className] = resultProbability
+        return classProbabilities
